0b3f8ca6a218c9a662238467ae8aa83991746073
Dmytriy Averyanov <dimthomas1@mail.ru>
2022-10-29 17:21:25 +0300

    Add Update, Enable and Delete for Product and view image on product's page

--- Admin/src/main/java/com/ecommerce/admin/controller/ProductController.java
+++ Admin/src/main/java/com/ecommerce/admin/controller/ProductController.java
@@ -2,15 +2,13 @@ package com.ecommerce.admin.controller;
 
 import com.ecommerce.library.dto.ProductDto;
 import com.ecommerce.library.model.Category;
+import com.ecommerce.library.model.Product;
 import com.ecommerce.library.service.CategoryService;
 import com.ecommerce.library.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -59,6 +57,58 @@ public class ProductController {
         }catch (Exception e){
             e.printStackTrace();
             attributes.addFlashAttribute("error", "Failed to add!");
+        }
+        return "redirect:/products";
+    }
+
+    @GetMapping("/update-product/{id}")
+    public String updateProductForm(@PathVariable("id") Long id, Model model, Principal principal) {
+        if(principal == null) {
+            return "redirect:/login";
+        }
+        model.addAttribute("title", "Update product");
+        List<Category> categories = categoryService.findAllByActivated();
+        ProductDto productDto = productService.getById(id);
+        model.addAttribute("categories", categories);
+        model.addAttribute("productDto", productDto);
+        return "update-product";
+    }
+
+    @PostMapping("/update-product/{id}")
+    public String processUpdate(@PathVariable("id") Long id,
+                                @ModelAttribute("productDto") ProductDto productDto,
+                                @RequestParam("imageProduct") MultipartFile imageProduct,
+                                RedirectAttributes attributes) {
+        try {
+            productService.update(imageProduct, productDto);
+            attributes.addFlashAttribute("success", "Update successfully!");
+        }catch (Exception e) {
+            e.printStackTrace();
+            attributes.addFlashAttribute("error", "Failed to update");
+        }
+        return "redirect:/products";
+    }
+
+    @RequestMapping(value = "/enable-product/{id}", method = {RequestMethod.PUT, RequestMethod.GET})
+    public String enabledProduct(@PathVariable("id") Long id, RedirectAttributes attributes) {
+        try {
+            productService.enableById(id);
+            attributes.addFlashAttribute("success", "Enabled successfully!");
+        }catch (Exception e) {
+            e.printStackTrace();
+            attributes.addFlashAttribute("error", "Failed to enabled!");
+        }
+        return "redirect:/products";
+    }
+
+    @RequestMapping(value = "/delete-product/{id}", method = {RequestMethod.PUT, RequestMethod.GET})
+    public String deletedProduct(@PathVariable("id") Long id, RedirectAttributes attributes) {
+        try {
+            productService.deleteById(id);
+            attributes.addFlashAttribute("success", "Deleted successfully!");
+        }catch (Exception e) {
+            e.printStackTrace();
+            attributes.addFlashAttribute("error", "Failed to deleted!");
         }
         return "redirect:/products";
     }
