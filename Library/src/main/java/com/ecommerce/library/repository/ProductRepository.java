05e0a7220b7ab0c2317bfcf8054b4eba974c2d78
Dmytriy Averyanov <dimthomas1@mail.ru>
2022-10-30 14:06:18 +0300

    Add pagination and searching for Produsts

--- Library/src/main/java/com/ecommerce/library/repository/ProductRepository.java
+++ Library/src/main/java/com/ecommerce/library/repository/ProductRepository.java
@@ -1,9 +1,17 @@
 package com.ecommerce.library.repository;
 
 import com.ecommerce.library.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Query("SELECT p FROM Product p")
+    Page<Product> pageProduct(Pageable pageable);
+
+    @Query("SELECT p FROM Product p WHERE p.description LIKE %?1% OR p.name LIKE %?1%")
+    Page<Product> searchProducts(String keyword, Pageable pageable);
 }
